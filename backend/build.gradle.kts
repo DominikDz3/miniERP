06efbfc3bfa78f67a48af1a586aca9e152ad92b0
Dominik Dziadosz <domix98@interia.pl>
2026-09-02 11:25:48 +0200

    fix: fixed wrong dependency, diffrent default credentials for database in application.yml and .env

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
 	runtimeOnly("org.postgresql:postgresql")
 
 	// Flyway
-	implementation("org.flywaydb:flyway-core")
+	implementation("org.springframework.boot:spring-boot-starter-flyway")
 	implementation("org.flywaydb:flyway-database-postgresql")
 
 	// Swagger
@@ -37,6 +37,10 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+	// Loombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
 
 	// Tests
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
